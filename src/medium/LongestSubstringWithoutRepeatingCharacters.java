d8d263b598312750f6724b58953820e1208d7cf9
xiaolong3000 <276947850@qq.com>
2017-10-16 22:37:53 +0800

    Runtime: 49 ms

--- src/medium/LongestSubstringWithoutRepeatingCharacters.java
+++ src/medium/LongestSubstringWithoutRepeatingCharacters.java
@@ -13,35 +13,85 @@ Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer 
 *
 * */
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class LongestSubstringWithoutRepeatingCharacters {
     public static void main(String[] args) {
-        String s="abcabcbb";
-       char[] r=s.toCharArray();
-       int max=0;
+        String s="pwwkew";
 
 
-        List<Character> list=new ArrayList<>();
-        List<Character> list_remove=new ArrayList<>();
-        for (int i=0;i<r.length;i++){
-          if (list.contains(r[i])){
-              max=max>=list.size()?max:list.size();
-               for (int j=0;j<=i;j++){
-                   list_remove.add(r[i]);
-               }
-               list.removeAll(list_remove);
-              list_remove.clear();
-          }
-          list.add(r[i]);
+        if ((null == s) || (s.length() == 0)) {
+           // return 0;
+        }
+        else if (s.length() == 1) {
+         //   return 1;
+        }
+        //好厉害啊，看其他人的答案Runtime: 49 ms
+        Set<Character> set = new HashSet<>();
+        int max = 0;
+        int index = 0;
+        int left = 0;
+        while (index < s.length()) {
+            if (set.add(s.charAt(index))) {
+                max = Math.max(max, ++index - left);
+            } else {
+                set.remove(s.charAt(left++));
+            }
         }
 
 
+
+
+
+
+
+
+
+        //较慢
+//       char[] r=s.toCharArray();
+//
+//       List<Character> list=new ArrayList<>();
+//       for (char c:r){
+//           for (int i=0;i<list.size();i++){
+//               if (list.get(i).equals(c)){
+//                   for (int j=i;j>=0;j--){
+//                       list.remove(j);
+//                   }
+//               }
+//           }
+//           list.add(c);
+//
+//           max=(max>list.size())?max:list.size();
+//       }
+//
 
         System.out.println(max);
 
 
 
     }
+
+
+    /*
+    *  List<Character> list=new ArrayList<>();
+ +       for (char c:r){
+ +
+ +          if (!list.contains(c)){
+ +              list.add(c);
+ +          }else {
+ +              for (int i=0;i<list.size();i++){
+ +                         if (list.get(i).equals(c)){
+ +                             for (int j=i;j>=0;j--){
+ +                                 list.remove(j);
+ +                             }
+ +                         }
+ +              }
+ +              list.add(c);
+ +          }
+ +           max=(max>list.size())?max:list.size();
+ +       }
+ +        System.out.println(max);
+    *
+    *
+    * */
 }
